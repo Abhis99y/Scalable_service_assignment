110df9d1e36008cdb05df25e9cd6108ffbd9c27d
Abhis99y <168298996+Abhis99y@users.noreply.github.com>
2024-11-24 10:51:35 +0530

    Deleting the files

--- InvoiceServiceApplication/src/main/java/com/scalableservices/assignement/invoice/repository/InvoiceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.scalableservices.assignement.invoice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.scalableservices.assignement.invoice.model.Invoice;
-
-public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
-}
